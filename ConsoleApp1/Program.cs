58f5ceb103901449a3b40752b96a73ded9ede09a
202-11 <202-11@202-11>
2023-05-28 13:09:41 +0300

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random r = new Random();
+            int sayi = r.Next(1, 100);
+            int i = 0;
+            while (i < 5)
+            {
+
+                Console.WriteLine("tahmininiz:");
+                int tahmin = Convert.ToInt32(Console.ReadLine());
+                if (tahmin < sayi)
+                {
+                    Console.WriteLine("tahmininizi büyültün");
+
+                }
+                else if (tahmin > sayi)
+                {
+                    Console.WriteLine("tahmininizi küçültün");
+                }
+                else if (tahmin == sayi)
+                {
+                    Console.WriteLine("Doğru tahmin");
+                    break;
+                }
+
+                i++;
+
+
+            }
+            Console.WriteLine("hakkınız bitti.");
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
